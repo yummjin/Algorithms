e9c358fdf7e58378aa41457812f1d2d8ccfbad40
한유진 <128016678+cho4u4o@users.noreply.github.com>
2025-01-10 01:35:23 +0900

    [Silver II] Title: 스택 수열, Time: 228 ms, Memory: 34272 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -10,20 +10,17 @@ const solution = () => {
 
 function stackToDest(n, dest) {
   let stack = [];
-  let result = [];
   let stringResult = [];
-  let current = 1; // 현재 추가할 숫자
+  let current = 1;
 
   for (let i = 0; i < n; i++) {
-    // 현재 목표 숫자에 도달할 때까지 스택에 추가
     while (current <= dest[i]) {
       stack.push(current++);
       stringResult.push("+");
     }
 
-    // 목표 숫자와 스택의 맨 위 요소 비교
     if (stack.pop() !== dest[i]) {
-      return "NO"; // 불가능한 경우
+      return "NO";
     }
     stringResult.push("-");
   }
