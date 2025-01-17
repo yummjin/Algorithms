eda7a264353e8dd1031e0b1c037fac34359edfb0
한유진 <128016678+cho4u4o@users.noreply.github.com>
2025-01-17 11:51:11 +0900

    [Bronze II] Title: Hashing, Time: 56 ms, Memory: 31256 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -8,4 +8,4 @@ inputs = list(map(ord, input()))
 nums = list(map(func_hashing, inputs))
 for i in range(0, N):
     num_pow.append(nums[i]*func_pow(i))
-print(sum(num_pow))
+print(sum(num_pow) % 1234567891)
