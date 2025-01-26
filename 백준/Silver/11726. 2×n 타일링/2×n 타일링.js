d7b3980d0ad6d95813dacbab660cc8b20e3e9a25
한유진 <128016678+cho4u4o@users.noreply.github.com>
2025-01-26 15:22:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 156 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? 0 : "input.txt";
+const input = fs.readFileSync(filePath, "utf8").toString().trim().split("\n");
+
+const solution = () => {
+  const N = parseInt(input[0]);
+  const dp = [0, 1, 2];
+  for (let i = 3; i <= N; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+  }
+  console.log(dp[N]);
+};
+
+solution();
